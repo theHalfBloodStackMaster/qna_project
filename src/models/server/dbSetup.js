2d10ed9b201775de38ac8693b0ebfc7cd8caa8f6
the half blood full stack master <amartotawar@gmail.com>
2026-08-20 14:26:11 +0530

    Implement getOrCreateDB function for database initialization and collection setup

---
+++ src/models/server/dbSetup.js
@@ -0,0 +1,32 @@
+import { db } from "../name.js";
+import createAnswerCollection from "./answer.collection.js";
+import createCommentCollection from "./comment.collection.js";
+import createQuestionCollection from "./question.collection.js";
+import createVoteCollection from "./vote.collection.js";
+import { databases } from "./config.js";
+
+export default async function getOrCreateDB() {
+  try {
+    // check for existing db
+    await databases.get(db);
+    console.log("Database connected");
+  } catch (error) {
+    try {
+      // create a new db
+      await databases.create(db, db);
+      console.log("Database created");
+      // create and excute all the collections
+      await Promise.all([
+        createAnswerCollection(),
+        createCommentCollection(),
+        createQuestionCollection(),
+        createVoteCollection(),
+      ]);
+      console.log("Collections are collected");
+      console.log("Database connected");
+    } catch (error) {
+      console.log("Error createing ddatabases or collection", error);
+    }
+  }
+  return databases;
+}
